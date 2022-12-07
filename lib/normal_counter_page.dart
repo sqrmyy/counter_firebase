344e0056b3c27d6d4291da7c47b01620c733f09a
sqrmyy <dn41.ra20adtdjw@gmail.com>
2022-12-07 18:21:38 +0900

    準備

---
+++ lib/normal_counter_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+import 'package:counter_firebase/main.dart';
+
+class NormalCounterPage extends ConsumerStatefulWidget {
+  const NormalCounterPage({Key? key}) : super(key: key);
+
+  @override
+  NormalCounterPageState createState() => NormalCounterPageState();
+}
+
+class NormalCounterPageState extends ConsumerState<NormalCounterPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = ref.watch(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homepage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(counterProvider.notifier).increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
